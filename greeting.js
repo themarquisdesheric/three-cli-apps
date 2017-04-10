ab31e0cb2ddfe71f6301374921ad729d8c48c783
Yuval Allweil <themarquisdesheric@gmail.com>
2017-04-10 16:13:12 -0700

    Function logic working

--- greeting.js
+++ greeting.js
@@ -1,7 +1,20 @@
 'use strict';
 
 function greet(name, time) {
-  return 'good morning';
+  const timeArray = time.split(':');
+  const hour = timeArray[0];
+  const minute = timeArray[1];
+  let timeOfDay;
+
+  if (hour <= 11 && minute <= 59) {
+    timeOfDay = 'morning';
+  } else if (hour <= 16 && minute <= 59) {
+    timeOfDay = 'afternoon';
+  } else {
+    timeOfDay = 'evening';
+  }
+  
+  return `good ${timeOfDay} ${name}`;
 }
 
 module.exports = greet;
